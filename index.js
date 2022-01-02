3b1898f99e21da754f98d0abb86ed534c8fa3dac
Jasmine <514269jasminezeng@gmail.com>
2022-01-02 13:00:39 +0800

    Updated .html and .css mainly on "Collection"

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+//Slideshow at #Collection
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// Next/previous controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
